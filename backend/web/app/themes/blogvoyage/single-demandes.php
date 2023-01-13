163dbc9d2fd0721417d1fdc92b2af0b437751b2a
LucasMachut <machut.lucas@gmail.com>
2023-01-13 13:16:18 +0100

    WIP responsive

---
+++ backend/web/app/themes/blogvoyage/single-demandes.php
@@ -0,0 +1,14 @@
+<?php
+get_header(); ?>
+
+
+<body>
+    <?php get_template_part('template_parts/header_menu'); ?>
+    <main>
+        <h1 class="text-danger">test Demandes template</h1>
+    </main>
+    <?php get_template_part('template_parts/insta-banner') ?>
+
+
+<?php get_footer();
+?>
